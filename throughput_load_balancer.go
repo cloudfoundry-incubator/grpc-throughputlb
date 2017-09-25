be65b5cff565c8937db8c21c1e477a4c54f963bf
Brady Love <blove@pivotal.io>
2017-09-25 07:46:06 -0600

    Move locking from address to load balancer
    * Add go doc documentation

--- throughput_load_balancer.go
+++ throughput_load_balancer.go
@@ -25,16 +25,12 @@ const (
 type address struct {
 	grpc.Address
 
-	mu             sync.RWMutex
 	state          addrState
 	activeRequests int
 	maxRequests    int
 }
 
 func (a *address) claim() error {
-	a.mu.Lock()
-	defer a.mu.Unlock()
-
 	if a.activeRequests >= a.maxRequests {
 		return errMaxRequestsExceeded
 	}
@@ -45,50 +41,38 @@ func (a *address) claim() error {
 }
 
 func (a *address) release() {
-	a.mu.Lock()
-	defer a.mu.Unlock()
-
 	a.activeRequests--
 }
 
 func (a *address) goUp() {
-	a.mu.Lock()
-	defer a.mu.Unlock()
-
 	a.state = stateUp
 }
 
+// TODO: As per the grpc-go documentation, it is not clear how to construct
+// and take advantage of the meaningful error parameter for down. Need
+// realistic demands to guide.
 func (a *address) goDown(_ error) {
-	a.mu.Lock()
-	defer a.mu.Unlock()
-
-	// TODO: Handle error
-
 	a.state = stateDown
 }
 
 func (a *address) isDown() bool {
-	a.mu.RLock()
-	defer a.mu.RUnlock()
-
 	return a.state == stateDown
 }
 
 func (a *address) capacity() int {
-	a.mu.RLock()
-	defer a.mu.RUnlock()
-
 	return a.activeRequests
 }
 
 func (a *address) atCapacity() bool {
-	a.mu.RLock()
-	defer a.mu.RUnlock()
-
 	return a.activeRequests >= a.maxRequests
 }
 
+// ThroughputLoadBalancer is a gRPC load balancer that satisfies the
+// grpc.Balancer interface. This load balancer will open multiple connections
+// to a single address and enfoce a maximum number of concurrent requests per
+// connection.
 type ThroughputLoadBalancer struct {
+	mu    sync.RWMutex
 	addrs []*address
 
 	target      string
@@ -97,6 +81,14 @@ type ThroughputLoadBalancer struct {
 	numAddrs    int
 }
 
+// NewThroughputLoadBalancer returns an initialized throughput load balancer
+// with the given number of maxRequests per address and max number of
+// addresses. This throughput load balancer can be passed to gRPC as a dial
+// option.
+//
+//	lb := throughputlb.NewThroughputLoadBalancer(100, 20)
+//	conn, err := grpc.Dial("localhost:8080", grpc.WithBalancer(lb))
+//	...
 func NewThroughputLoadBalancer(
 	maxRequests int,
 	numAddrs int,
@@ -111,7 +103,14 @@ func NewThroughputLoadBalancer(
 	return lb
 }
 
+// Start is called by gRPC when dial is called to configure the load balancer
+// with a target and a BalancerConfig. The BalancerConfig is not used by this
+// load balancer. When start is called by gRPC the load balancer will be
+// configured with a configured number of addresses and notify gRPC of those
+// addresses. All of the address targets are the same with an integer as
+// metidata to make them unique.
 func (lb *ThroughputLoadBalancer) Start(target string, cfg grpc.BalancerConfig) error {
+	lb.mu.Lock()
 	lb.target = target
 	for i := 0; i < lb.numAddrs; i++ {
 		lb.addrs[i] = &address{
@@ -122,18 +121,24 @@ func (lb *ThroughputLoadBalancer) Start(target string, cfg grpc.BalancerConfig) 
 			maxRequests: lb.maxRequests,
 		}
 	}
-
 	lb.sendNotify()
+	lb.mu.Unlock()
 
 	return nil
 }
 
+// Up is called by gRPC when a connection has been established for the given
+// address.
 func (lb *ThroughputLoadBalancer) Up(addr grpc.Address) func(error) {
+	lb.mu.RLock()
 	addrs := lb.addrs
+	lb.mu.RUnlock()
 
 	for _, a := range addrs {
 		if a.Address == addr {
+			lb.mu.Lock()
 			a.goUp()
+			lb.mu.Unlock()
 
 			return a.goDown
 		}
@@ -142,19 +147,34 @@ func (lb *ThroughputLoadBalancer) Up(addr grpc.Address) func(error) {
 	return func(_ error) {}
 }
 
+// Get is called by gRPC when your client initiates a request. Up will return
+// find the least used address and return that for a client to use. If
+// BlockingWait is true on the BalancerGetOptions this method will block until
+// an address is available, otherwise an error will be returned if no address
+// have any available capacity.
 func (lb *ThroughputLoadBalancer) Get(ctx context.Context, opts grpc.BalancerGetOptions) (grpc.Address, func(), error) {
 	addr, err := lb.next(opts.BlockingWait)
 	if err != nil {
 		return grpc.Address{}, func() {}, err
 	}
 
-	return addr.Address, addr.release, nil
+	r := func() {
+		lb.mu.Lock()
+		addr.release()
+		lb.mu.Unlock()
+	}
+
+	return addr.Address, r, nil
 }
 
+// Notify returns a channel that is used to notify gRPC of changes to the
+// address set.
 func (lb *ThroughputLoadBalancer) Notify() <-chan []grpc.Address {
 	return lb.notify
 }
 
+// Close is called by gRPC when a connection is being closed. For this load
+// balancer it is a no-op.
 func (*ThroughputLoadBalancer) Close() error {
 	return nil
 }
@@ -173,6 +193,7 @@ func (lb *ThroughputLoadBalancer) next(wait bool) (*address, error) {
 		var addr *address
 		lowestCapacity := lb.maxRequests * 2
 
+		lb.mu.Lock()
 		for _, a := range lb.addrs {
 			if a.isDown() || a.atCapacity() {
 				continue
@@ -187,9 +208,11 @@ func (lb *ThroughputLoadBalancer) next(wait bool) (*address, error) {
 		if addr != nil {
 			err := addr.claim()
 			if err == nil {
+				lb.mu.Unlock()
 				return addr, nil
 			}
 		}
+		lb.mu.Unlock()
 
 		if !wait {
 			return nil, errUnavailable
